c4c2aa8371580008f19ec114667d2b8064d16a4d
uri <uri.valevski@gmail.com>
2023-03-15 01:32:44 +0200

    add-export

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,6 @@
 import { connectWithAcking } from "./acking.ts";
 import { crypto } from "../../common/src/index.ts";
 
+export const getPublicKey = crypto.getPublicKey;
 export const generatePrivateKey = crypto.generatePrivateKey;
 export const connect = connectWithAcking;
