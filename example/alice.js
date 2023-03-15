9cf4598efe7e23d0b6cad81f7a5b38328151f1ce
uri <uri.valevski@gmail.com>
2023-03-15 11:39:23 +0200

    add e2e npm example

---
+++ example/alice.js
@@ -0,0 +1,22 @@
+const { connect, getPublicKey } = require("message-portal");
+
+const alice =
+  "a0df4297637a6d0087414e6cb8d295f4284a42fb1da38362ee0c17e31765dd03";
+const bob = "f353ce7b4df97a8db6ec6dc2acafee09b4e84848a1fd0f9a1066fa926112f791";
+
+connect({
+  privateKey: alice,
+  onMessage: ({ from, payload }) =>
+    Promise.resolve(console.log(`Bob (${from}) says`, payload)),
+  onClose: () => {},
+}).then(({ send }) => {
+  setInterval(() => {
+    console.log("sending");
+    send({
+      to: getPublicKey(bob),
+      payload: "hello from Alice.",
+    }).then(() => {
+      console.log("Bob has acked!");
+    });
+  }, 5000);
+});
---
+++ example/bob.js
@@ -0,0 +1,22 @@
+const { connect, getPublicKey } = require("message-portal");
+
+const alice =
+  "a0df4297637a6d0087414e6cb8d295f4284a42fb1da38362ee0c17e31765dd03";
+const bob = "f353ce7b4df97a8db6ec6dc2acafee09b4e84848a1fd0f9a1066fa926112f791";
+
+connect({
+  privateKey: bob,
+  onMessage: ({ from, payload }) =>
+    Promise.resolve(console.log(`Alice (${from}) says`, payload)),
+  onClose: () => {},
+}).then(({ send }) => {
+  setInterval(() => {
+    console.log("sending");
+    send({
+      to: getPublicKey(alice),
+      payload: "hello from bob.",
+    }).then(() => {
+      console.log("Alice has acked!");
+    });
+  }, 5000);
+});
