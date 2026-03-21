540d70740970d792f68bae09c328afcf5302bd66
Uri <uri.valevski@gmail.com>
2026-03-21 17:15:48 +0200

    pin secp256k1 to @1.7.2 to fix broken deploy

--- common/src/crypto.ts
+++ common/src/crypto.ts
@@ -1,5 +1,5 @@
 import * as base64 from "https://denopkg.com/chiefbiiko/base64@v0.2.1/mod.ts";
-import * as secp256k1 from "https://deno.land/x/secp256k1/mod.ts";
+import * as secp256k1 from "https://deno.land/x/secp256k1@1.7.2/mod.ts";
 
 import { sha256 } from "https://denopkg.com/chiefbiiko/sha256@v1.0.0/mod.ts";
 
