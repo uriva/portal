b0033e20082ba7c470cb719899f2ece0e7c7ad13
Uri <uri.valevski@gmail.com>
2026-03-25 18:27:54 +0200

    fix: update default hub url to uriva-portal.deno.dev

--- client/src/connect.ts
+++ client/src/connect.ts
@@ -64,7 +64,7 @@ const encryptAndSign =
       signEncryptedMessage(privateKey, to),
     );
 
-const DEFAULT_HUB_URL = "wss://uriva-portal-mete6t9t7geg.deno.dev/";
+const DEFAULT_HUB_URL = "wss://uriva-portal.deno.dev/";
 const DEFAULT_MAX_RECONNECT_DELAY_MS = 30_000;
 const INITIAL_RECONNECT_DELAY_MS = 500;
 
